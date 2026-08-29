ae1f56db0029390d6cf3d28f0e1b6f9a07fecf99
cunxpofazatron <cunxpofazatron@gmail.com>
2026-08-29 13:27:59 +0300

    Security: настроено ограничение частоты запросов (Throttling) к API

--- orders/settings.py
+++ orders/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default-unsafe-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -145,7 +145,17 @@ REST_FRAMEWORK = {
     # Добавляем пагинацию:
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+
+    # Защита от спама (Throttling)
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',   # Лимит для неавторизованных: 100 запросов в день
+        'user': '1000/day'   # Лимит для авторизованных: 1000 запросов в день
+    }
 }
 
 SPECTACULAR_SETTINGS = {
