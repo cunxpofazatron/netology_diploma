cc43baef385a072bdfec15d4c8c5fc47aa8906ab
cunxpofazatron <cunxpofazatron@gmail.com>
2026-08-29 12:39:06 +0300

    Test: добавлены базовые автотесты для моделей приложения

--- backend/tests.py
+++ backend/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Category, Shop
 
-# Create your tests here.
+class ModelsTestCase(TestCase):
+    def test_category_creation(self):
+        """Проверка создания категории товаров"""
+        category = Category.objects.create(name='Электроника')
+        self.assertEqual(category.name, 'Электроника')
+
+    def test_shop_creation(self):
+        """Проверка создания магазина"""
+        shop = Shop.objects.create(name='Связной', state=True)
+        self.assertEqual(shop.name, 'Связной')
+        self.assertTrue(shop.state)
